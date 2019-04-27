a3ee9891154e3f62e84e96ee8647fae8774ed66c
Michał Suliborski <msuliborski@gmail.com>
2019-04-27 20:02:50 +0200

    effwe

---
+++ Assets/Scripts/BaseScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseScript : MonoBehaviour {
+    
+    public int health;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
