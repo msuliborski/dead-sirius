2f220b925cd5be343d86c7e5435858f3c14b7b5b
jedrek <jedrekszor@wp.pl>
2019-04-28 13:51:09 +0200

    fuisgfdh

---
+++ Assets/Scripts/Lifecount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Lifecount : MonoBehaviour
+{
+    private PlayerControllsNodes _player;
+    private AINode _ai;
+    [SerializeField] private TextMeshProUGUI _tmPlayer;
+    [SerializeField] private TextMeshProUGUI _tmEnemy;
+    void Start()
+    {
+        _player = GameObject.Find("PlayerNode").GetComponent<PlayerControllsNodes>();
+        _ai = GameObject.Find("EnemyNode").GetComponent<AINode>();
+    }
+
+    void Update()
+    {
+        _tmPlayer.text = _player.health.ToString();
+        _tmEnemy.text = _ai.health.ToString();
+    }
+}
