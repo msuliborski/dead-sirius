bf9201a03b00312725878b09adb7bbc8be0badb0
jedrek <jedrekszor@wp.pl>
2019-04-28 10:20:33 +0200

    bowman

---
+++ Assets/Scripts/Blow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blow : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(blow());
+    }
+
+    IEnumerator blow()
+    {
+        yield return new WaitForSeconds(4);
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Mob"))
+        {
+            if (other.gameObject.GetComponent<MobBehaviour>().ownerId != 1)
+                other.gameObject.GetComponent<MobBehaviour>().health = 0;
+        }
+    }
+}
