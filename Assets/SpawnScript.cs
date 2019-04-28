3655419ab22fd99f96a4d7ade0263f00e2799e5c
Michał Suliborski <msuliborski@gmail.com>
2019-04-28 12:26:52 +0200

    wad

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   // public bool block;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //block = false;
+    }
+
+    private void OnTriggerStay(Collider other) {
+       // block = true;
+    }
+    
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Mob")) 
+//            other.GetComponent<MobBehaviour>().GetComponent<PlayerControllsNodes>().Blocked = false;
+//        else if (other.CompareTag("Mob"))
+//            other.GetComponent<MobBehaviour>().GetComponent<AINode>().Blocked = false;
+//
+//    }
+}
